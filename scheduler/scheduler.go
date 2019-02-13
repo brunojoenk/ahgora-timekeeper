0b44d302a4f3652b533ca9b47f29dc5493d5a380
Roger Fernandes <roger.osbourne@gmail.com>
2019-02-13 10:10:23 -0200

    Changing scheduler times

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -8,8 +8,8 @@ import (
 //StartScheduler - Starts scheduling point punchers
 func StartScheduler(service *service.Service) {
 	gocron.Every(1).Day().At("08:00").Do(service.PunchPoint)
-	gocron.Every(1).Day().At("12:00").Do(service.PunchPoint)
-	gocron.Every(1).Day().At("13:30").Do(service.PunchPoint)
-	gocron.Every(1).Day().At("18:00").Do(service.PunchPoint)
+	gocron.Every(1).Day().At("11:53").Do(service.PunchPoint)
+	gocron.Every(1).Day().At("13:04").Do(service.PunchPoint)
+	gocron.Every(1).Day().At("18:23").Do(service.PunchPoint)
 	gocron.Start()
 }
