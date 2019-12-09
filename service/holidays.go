335166d968505d985bab22ebf0f08fa71c8799fb
Bruno Joenk <bjoenk@gmail.com>
2019-12-09 15:09:33 -0300

    Adicionado datas comemorativas

--- service/holidays.go
+++ service/holidays.go
@@ -23,16 +23,25 @@ const (
 	//ProclamationOfTheRepublic - Proclamação Da República
 	ProclamationOfTheRepublic = "2019-11-15"
 
+	//Festa de encerramento
+	CompanyParty = "2019-12-13"
+
 	//ChristmasEve - Véspera de Natal
 	ChristmasEve = "2019-12-24"
 
 	//Christmas - Natal
 	Christmas = "2019-12-25"
+
+	//End's year
+	EndsYear = "2019-12-31"
+
+	//New's year
+	NewsYear = "2020-01-01"
 )
 
 func isHoliday(day time.Time) bool {
 	switch day.Format(defaultFormat) {
-	case CarnivalOptionalPoint, Carnival, GoodFriday, MayDay, CorpusChristi, ProclamationOfTheRepublic, ChristmasEve, Christmas:
+	case CarnivalOptionalPoint, Carnival, GoodFriday, MayDay, CorpusChristi, ProclamationOfTheRepublic, CompanyParty, ChristmasEve, Christmas, EndsYear, NewsYear:
 		return true
 	default:
 		return false
